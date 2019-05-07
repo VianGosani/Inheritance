d64f86c7d968b6048e9b36e1829b4a8c7e9cdb8b
VianGosani <48441043+VianGosani@users.noreply.github.com>
2019-05-07 12:05:38 +0700

    Add files via upload

---
+++ Inheritance/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+   public class Teacher : Person
+    {
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+        
+        public Teacher(string name, int age, string id, string subjek) : base(name,age)
+        {
+            id = TeacherId;
+            subjek = Subject;
+        }
+    }
+}
